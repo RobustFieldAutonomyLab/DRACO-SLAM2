c06ff1ebe298eae737410ee24e6f90b9a08e7267
jake3991 <you@example.com>
2023-04-27 09:19:34 -0400

    Adding covariance prediction to registration

--- registration.py
+++ registration.py
@@ -2,6 +2,8 @@ import gtsam
 import numpy as np
 
 from icp import GI_ICP
+from sklearn.covariance import MinCovDet
+from scipy.spatial.transform import Rotation
 from utils import numpy_to_gtsam, transform_points, rot_to_euler
 from loop_closure import LoopClosure
 
@@ -11,6 +13,53 @@ class Registration():
 
     def __init__(self,sampling_points,iterations,tolerance,max_translation,max_rotation) -> None:
         self.icp = GI_ICP(sampling_points,iterations,tolerance,max_translation,max_rotation)
+
+    def predict(self,points:np.array,samples:int) -> np.array:
+        """Predict the covariance if we call ICP on this point cloud
+
+        Args:
+            points (np.array): the point cloud
+            samples (int): the number of samples to get a cov matrix
+
+        Returns:
+            np.array: the 3x3 covariance matrix
+        """
+
+        arr = []
+        
+        # loop to generate some samples
+        for j in range(samples):
+
+            # define the clouds
+            source_cloud = np.array(points)
+            target_cloud = np.array(points)
+            
+            # check that the source cloud has any points
+            if len(source_cloud) > 1:
+
+                dx = np.random.normal(0,1.0) # generate a random x
+                dy = np.random.normal(0,1.0) # generate a random y
+                da = np.random.normal(0,np.radians(1.0)) # generate a random theta
+                step = gtsam.Pose2(dx,dy,da) # compile as a gtsam pose
+                T = step.matrix().astype(np.float32) # convert to a matrix
+                source_cloud = source_cloud.dot(T[:2, :2].T) + T[:2, 2] # apply the transform to the source cloud
+
+                # get the ICP transform
+                _, T = self.icp.refine(source_cloud,target_cloud)
+                source_cloud = source_cloud.dot(T[:2, :2].T) + T[:2, 2]
+
+                # log the ICP output for covariance estimation
+                r1 = T[:2, :2].T
+                r = np.eye(3)
+                r[0][0], r[0][1], r[1][0], r[1][1] = r1[0][0], r1[0][1], r1[1][0], r1[1][1]
+                arr.append([T[1][2],T[0][2],Rotation.from_matrix(r).as_euler("xyz")[2]])
+
+        # check if the results can support covariance estimation
+        if(len(arr) > 2):
+            cov = MinCovDet(random_state=1).fit(arr).covariance_
+            return cov
+        else:
+            return None
 
     def evaluate(self,loop:LoopClosure,
                  min_points:int, ratio_points:float, 
