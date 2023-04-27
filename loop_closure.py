9054d9b6d8e8da8099886e04da29bece19a6acf3
jake3991 <you@example.com>
2023-04-27 09:19:01 -0400

    Adding keys to loop closure

--- loop_closure.py
+++ loop_closure.py
@@ -3,7 +3,9 @@ class LoopClosure():
     """A class to store all the info needed for a loop closure
     """
     
-    def __init__(self,source_points,target_points,source_context,target_context) -> None:
+    def __init__(self,source_key,target_key,source_points,target_points,source_context,target_context) -> None:
+        self.source_key = source_key
+        self.target_key = target_key
         self.source_points = source_points
         self.target_points = target_points
         self.status = True
