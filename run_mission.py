82a6a5044a03c89d6618a69b3688d6d16d701898
jake3991 <you@example.com>
2023-05-29 16:19:13 -0400

    Code cleanup in the main loop

--- run_mission.py
+++ run_mission.py
@@ -45,77 +45,56 @@ for slam_step in range(63):
     # search for loop closures
     for robot_id_source in robots.keys():
         for robot_id_target in robots.keys():
+            if robot_id_target == robot_id_source: continue # do not search with self
+
             # TODO account for comms cost here
             robots[robot_id_source].update_partner_trajectory(robot_id_target,robots[robot_id_target].state_estimate)
 
-            if robot_id_target == robot_id_source: continue # do not search with self
-            loops = search_for_loops(reg,robots,comm_link,robot_id_source,robot_id_target,
-                                     MIN_POINTS,RATIO_POINTS,CONTEXT_DIFFERENCE,MIN_OVERLAP,MAX_TREE_DIST,KNN)
-            # loops = reject_loops(loops,MIN_POINTS,RATIO_POINTS,CONTEXT_DIFFERENCE,MIN_OVERLAP)
-            # loops = grade_loop_list(loops,max_correct_distance,max_correct_rotation)
-
+            # perform some loop closure search
+            loops = search_for_loops(reg,
+                                     robots,
+                                     comm_link,
+                                     robot_id_source,
+                                     robot_id_target,
+                                     MIN_POINTS,
+                                     RATIO_POINTS,
+                                     CONTEXT_DIFFERENCE,
+                                     MIN_OVERLAP,
+                                     MAX_TREE_DIST,
+                                     KNN)
+            
+            # only keep going if we found any loop closures
             if len(loops) == 0: continue
-            loop_, loop_search_status = keep_best_loop(loops)
+            loop_, loop_search_status = keep_best_loop(loops) # retain only the best loop from the batch
             if loop_search_status == False: continue # make sure the best loop not outlier
+
+            # do some book keeping
             loop_.place_loop(robots[robot_id_source].get_pose_gtsam())
             loop_.source_robot_id = robot_id_source
             loop_.target_robot_id = robot_id_target
+
+            # check the status of the loop closure
             if loop_.status:
+                
                 # update and solve PCM
                 robots[robot_id_source].add_loop_to_pcm_queue(loop_)
                 valid_loops = robots[robot_id_source].do_pcm(robot_id_target)
 
                 # if we have a valid solution from PCM, merge the graphs
                 if len(valid_loops) > 0:
-                    robots[robot_id_source].merge_slam(valid_loops)
-                    flipped_valid_loops = flip_loops(valid_loops)
+                    robots[robot_id_source].merge_slam(valid_loops) # merge my graph
+                    flipped_valid_loops = flip_loops(valid_loops) # flip and send the loops
                     for i in range(len(flipped_valid_loops)): comm_link.log_message(96 + 16 + 16)
-                    robots[robot_id_target].merge_slam(flipped_valid_loops)
+                    robots[robot_id_target].merge_slam(flipped_valid_loops) # merge the partner robot graph
                     
                 for valid in valid_loops: 
                     loop_list.append(valid)
-                    robots[robot_id_source].plot()
+                    # robots[robot_id_source].plot()
                     # robots[robot_id_target].plot()
                     
+# plot each of the robots
 for robot in robots.keys():
     robots[robot].plot()
-
-'''comm_link.plot()
-
-temp = robots[1].truth
-poses_one = []
-for row in temp:
-    poses_one.append([row.x(),row.y()])
-poses_one = np.array(poses_one)
-
-temp = robots[2].truth
-poses_two = []
-for row in temp:
-    poses_two.append([row.x(),row.y()])
-poses_two = np.array(poses_two)
-
-plt.figure(figsize=(20,10))
-plt.scatter(poses_one[:,0],-poses_one[:,1],c="blue")
-plt.plot(poses_one[:,0],-poses_one[:,1],c="blue")
-plt.scatter(poses_two[:,0],-poses_two[:,1],c="red")
-plt.plot(poses_two[:,0],-poses_two[:,1],c="red")
-
-for loop in loop_list:
-    i,j = loop.source_key,loop.target_key
-    source_robot, target_robot = loop.source_robot_id,loop.target_robot_id
-    print(source_robot,target_robot)
-
-    x1,y1 = robots[source_robot].truth[i].x(), robots[source_robot].truth[i].y()
-    x2,y2 = robots[target_robot].truth[j].x(), robots[target_robot].truth[j].y()
-    
-    plt.plot([x1,x2],[-y1,-y2],color="black")
-    plt.scatter([x1],[-y1],color="black",marker="*",s=150)
-    plt.scatter([x2],[-y2],color="black",marker="*",s=150)
-
-
-plt.axis("square")
-plt.show()'''
-
 
 
 
