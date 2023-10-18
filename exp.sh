705a2125a8b94c704b9497d2bd47ae19b771c9ba
jake3991 <you@example.com>
2023-10-18 09:50:51 -0400

    param update

--- exp.sh
+++ exp.sh
@@ -1,7 +1,7 @@
 # python run_mission.py plane 1 80 0 0.87 .005
 # python run_mission.py plane 1 80 1 0.87 .005
 # python run_mission.py usmma 100 60 0 0.60 .005
-python run_mission.py usmma 100 60 1 0.60 .0056
+python run_mission.py usmma 1 60 0 0.60 .0056
 # python run_mission.py usmma_real 100 103 1 0.55 .00025
 # python run_mission.py usmma_real 100 103 0 0.55 .00025
 
