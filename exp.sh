844e82888ebec243a15b634f902cf287c9623897
jake3991 <you@example.com>
2024-07-22 09:15:50 -0400

    Adding some sample commands to run the system

--- exp.sh
+++ exp.sh
@@ -1,7 +1,13 @@
-# python run_mission.py plane 1 80 0 0.87 .005
-# python run_mission.py plane 1 80 1 0.87 .005
-# python run_mission.py usmma 100 60 0 0.60 .005
-python run_mission.py usmma 1 60 0 0.60 .0056
-# python run_mission.py usmma_real 100 103 1 0.55 .00025
-# python run_mission.py usmma_real 100 103 0 0.55 .00025
+# some sample commands
+# python run_mission.py 0 plane 1 80 0 0.87 .015
+# python run_mission.py 0 plane 1 80 1 0.87 .015
+
+# python run_mission.py usmma 1 60 0 0.60 .015
+# python run_mission.py usmma 1 60 1 0.60 .015
+
+# python run_mission.py usmma 1 60 0 0.60 .005
+# python run_mission.py usmma 1 60 0 0.60 .0056
+
+python run_mission.py usmma_real 1 103 1 0.55 .015
+python run_mission.py usmma_real 1 103 0 0.55 .015
 
